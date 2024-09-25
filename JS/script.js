f8bf18ab3cf94a6ea46d9bbb8c9d29e9521be872
yusrilihza-m <yusrilihza572@gmail.com>
2024-09-25 11:15:36 +0700

    tugas mini project akhir 2

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,4 @@
 
-var myIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("myslides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2000);
-}
-
 function validateForm() {
   const nama = document.forms['nama-anda']['name-input'].value
 
@@ -31,4 +16,38 @@ function setName(name) {
   document.getElementById('error-name').innerHTML = "";
 
   return false;
+}
+function validateMessage(){
+  const name = document.forms["message-form"]["full-name"].value
+  const birthdate = document.forms["message-form"]["birth-date"].value
+  const gender = document.forms["message-form"]["gender"].value
+  const messages = document.forms["message-form"]["messages"].value
+
+  if (name == "" || birthdate == "" || gender == "" || messages == ""){
+    alert("Tidak boleh kosong!");
+    return false;
+  }
+
+  setSenderUI(name, birthdate, gender, messages)
+  return false;
+}
+
+function setSenderUI(name, birthdate, gender, messages){
+  document.getElementById("sender-full-name").innerHTML = name;
+  document.getElementById("sender-birth-date").innerHTML = birthdate;
+  document.getElementById("sender-gender").innerHTML = gender;
+  document.getElementById("sender-messages").innerHTML = messages;
+}
+var myIndex = 0;
+carousel();
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("myslides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000);
 }
